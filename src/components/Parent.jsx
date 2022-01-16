1bd820e1f86515963dc1f5696258aa50446c8dd3
Rick_Alves <henryworkdev.ti@gmail.com>
2022-01-16 20:59:26 -0300

    feature cloneElement

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 
-import Child from './Child'
+export default props =>
 
-export default props => 
     <div>
         <h1>{props.firstName} {props.lastName}</h1>
         <h2>Children</h2>
         <ul>
-            <Child firstName="Peter" lastName={props.lastName} />
-            <Child {...props}/>
-            <Child {...props} firstName="Louise"/>
-            <Child {...props} lastName="Cruze Blade"/>
+            {/*Imput childs via component*/}
+            {/* {props.children} */}
+            {/* This function create a inheritance passing the lastName to every
+                child of a parent*/}
+            {
+                React.Children.map(props.children, child => {
+                    return React.cloneElement(child, { 
+                        ...props, ...child.props 
+                    })
+                })  
+            }
+           
         </ul>
     </div>
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import ReactDOM from 'react-dom'
 // import Multiple from './components/Multiple'
 // import Hello from './components/Hello'
 import Mother from './components/Parent'
+import Child from './components/Child'
 
 ReactDOM.render(
     // <div>
@@ -27,7 +28,11 @@ ReactDOM.render(
     //     <Hello type="Good Morning" name = "Rick" />
     // </div>,
     <div>
-        <Mother firstName="Mary" lastName="Zunkenbarg Cruze"/>
+        <Mother firstName="Mary" lastName="Wick">
+            <Child firstName="John" />
+            <Child firstName="Vanessa" />
+            <Child firstName="Josef" />
+        </Mother>
     </div>,
      document.getElementById('root')
 )
